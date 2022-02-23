85b6ad15510f84713992b3ea4325dd1bfdd97de9
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-02-23 09:18:27 +0100

    Modificado EJ 6

--- 6.- Ocultado Lista Completa/script.js
+++ 6.- Ocultado Lista Completa/script.js
@@ -1,5 +1,5 @@
 $(document).ready(main) 
 
 function main() {
-    $('button').click(function() { $("ul ul:first").hide() })
+    $('button').click(function() { $("ul ul li:nth-child(1)").hide() })
 }
