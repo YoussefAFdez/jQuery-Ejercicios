cd948a4b431ee30aed13429188e50d13bfd456e6
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-03-10 17:06:37 +0100

    Función Each sin terminar

---
+++ #32.- Funcion Each/script.js
@@ -0,0 +1,14 @@
+$(document).ready(main) 
+
+function main() {
+    /**
+     * btn1 = 
+     * btn2 = 
+     */
+
+    $('#btn1').click(() => $('#parrafo').text('Hola Mundo'));
+    $('#btn2').click(() => $('#parrafo').text('Hola Mundo'));
+
+}
+
+
