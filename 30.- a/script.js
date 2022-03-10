4ff3efd3e0ddc24583740a08a5464553959dcbe8
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-03-10 16:53:33 +0100

    Ejercicio 30 - Botones Alert

--- 30.- a/script.js
+++ 30.- a/script.js
@@ -1,12 +1,18 @@
 $(document).ready(main) 
 
 function main() {
-    // $('#activador').click(() => $('#efecto').css('color', 'red').slideUp(2000).slideDown(2000));
-    $('#activador').click(() => $('#efecto')
-        .css('color', 'red')
-        .slideUp(2000)
-        .slideDown(2000)
-    );
+    /**
+     * btn1 = Carga un alert con el contenido del parrafo
+     * btn2 = Carga un alert con el HTML del parrafo
+     * btn3 = Carga un alert con el value de un textbox
+     * btn4 = Carta un alert con el contenido del enlace
+     */
+
+    $('#btn1').click(() => alert($('#sinopsis').text()));
+    $('#btn2').click(() => alert($('#sinopsis').prop('outerHTML')));
+    $('#btn3').click(() => alert($('#tbTexto').val()));
+    $('#btn4').click(() => alert($('#lnHobbit').prop('href')));
+
 }
 
 
